0f535f9cb71399075c60a9f4ab3513e16c3aca6b
iamritik <ritikraj21april@gmail.com>
2023-06-29 15:36:06 +0530

    4 balls

--- src/test/java/TryProcessing.java
+++ src/test/java/TryProcessing.java
@@ -5,12 +5,24 @@ public class TryProcessing extends PApplet {
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
     public static final int DIAMETER = 10;
-    int x1 = 0, x2 = 0, x3 = 0, x4 = 0;
-
+    private Circle circle1,circle2,circle3,circle4;
     public static void main(String[] args){
         PApplet.main("TryProcessing", args);
     }
+    public class Circle {
+        private int x,height,speed;
 
+        public Circle(int x, int height, int speed) {
+            this.x = x;
+            this.height = height;
+            this.speed = speed;
+        }
+
+        public void display() {
+            ellipse(x, height, DIAMETER, DIAMETER);
+            x += speed;
+        }
+    }
     @Override
     public void settings() {
         super.settings();
@@ -18,29 +30,21 @@ public class TryProcessing extends PApplet {
     }
 
     @Override
-    public void draw() {
-//        paintWhite();
-        circle1();
-        circle2();
-        circle3();
-        circle4();
+    public void setup() {
+        paintWhite();
+        circle1 = new Circle(0, HEIGHT/5, 1);
+        circle2 = new Circle(0, 2*HEIGHT/5, 2);
+        circle3 = new Circle(0, 3*HEIGHT/5, 3);
+        circle4 = new Circle(0, 4*HEIGHT/5, 4);
     }
 
-    private void circle1() {
-        ellipse(x1,HEIGHT/5,DIAMETER, DIAMETER);
-        x1++;
-    }
-    private void circle2() {
-        ellipse(x2,2*HEIGHT/5,DIAMETER, DIAMETER);
-        x2+=2;
-    }
-    private void circle3() {
-        ellipse(x3,3*HEIGHT/5,DIAMETER, DIAMETER);
-        x3+=3;
-    }
-    private void circle4() {
-        ellipse(x4,4*HEIGHT/5,DIAMETER, DIAMETER);
-        x4+=4;
+    @Override
+    public void draw() {
+//        paintWhite();
+        circle1.display();
+        circle2.display();
+        circle3.display();
+        circle4.display();
     }
 
     private void paintWhite() {
